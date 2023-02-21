675de2258b41056b9c71093a11d0094e39fa65c8
FubyCutie <roset280905@gmail.com>
2023-02-21 08:13:49 +0000

    fixed overzealous hopper checking

--- src/main/java/com/blackoutburst/workshop/core/events/spigot/OpenHopper.java
+++ src/main/java/com/blackoutburst/workshop/core/events/spigot/OpenHopper.java
@@ -1,12 +1,17 @@
 package com.blackoutburst.workshop.core.events.spigot;
 
+import com.blackoutburst.workshop.core.WSPlayer;
 import org.bukkit.Material;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class OpenHopper {
     public static void execute(PlayerInteractEvent event) {
         if (event.getClickedBlock() == null) return;
         if (event.getClickedBlock().getType() != Material.HOPPER) return;
+        if (WSPlayer.getFromPlayer(event.getPlayer()) == null) return;
+        if (!WSPlayer.getFromPlayer(event.getPlayer()).isInGame()) return;
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
 
         event.setCancelled(true);
     }
